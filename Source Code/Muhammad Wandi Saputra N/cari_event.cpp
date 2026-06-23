0a8eac03fd18ca1d8787cc0bfd777a876b0b55eb
MuhammadWandiSaputraNurhasan <2503010050@unper.ac.id>
2026-06-23 19:09:50 +0700

    Update cari_event.cpp

--- Source Code/Muhammad Wandi Saputra N/cari_event.cpp
+++ Source Code/Muhammad Wandi Saputra N/cari_event.cpp
@@ -1,1 +1,15 @@
+void cariEvent(string cari) {
+    Event* temp = head;
 
+    while(temp != NULL) {
+        if(temp->nama == cari) {
+            cout << "\nEvent ditemukan\n";
+            cout << "Nama    : " << temp->nama << endl;
+            cout << "Tanggal : " << temp->tanggal << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+
+    cout << "Event tidak ditemukan!\n";
+}
