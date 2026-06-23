a5719c89d1559680afdcea91088f46bd48354849
2503010073-Muhamadatardiyansahk <2503010073@unper.ac.id>
2026-06-23 19:01:57 +0700

    Add files via upload

---
+++ Source Code/Muhamad Atardiyansyahk/strukturpanitia.cpp
@@ -0,0 +1,11 @@
+void tampilPanitia() {
+
+    cout << "\nStruktur Kepanitiaan\n";
+    cout << "Ketua      : Wandi\n";
+    cout << "Sekretaris : Abian\n";
+    cout << "Bendahara  : Rafi\n";
+    cout << "Divisi Acara\n";
+    cout << "Divisi Konsumsi\n";
+    cout << "Divisi Dokumentasi\n";
+
+}
